15f814e00bcf3c300099c3097ccf12f616b1531b
DarkLordZeus <gauravsinghraikwal13@gmail.com>
2026-04-04 19:54:33 +0530

    aa

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/muskaanPortfolio">
+    <App />
+  </BrowserRouter>
+);
